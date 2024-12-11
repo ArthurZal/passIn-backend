362f88c44057b700275329464f250e6e9013a86a
Arthur Zall <zallarthur@gmail.com>
2024-12-11 04:27:00 -0300

    Validation schema created to handle user data received in the events route

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
 import fastify from 'fastify'
+import { title } from 'process'
+import { z } from 'zod'
 
 const app = fastify()
 
 app.post('/events', async (request, reply) => {
+    const createSchema = z.object({
+        title: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable()
+    })
     const data = request.body
 
      return {message: 'Dados recebidos!', receivedData: data};
 
-})
-
-app.get('/teste', ()=>{
-    return 'Rota de teste'
 })
 
 
